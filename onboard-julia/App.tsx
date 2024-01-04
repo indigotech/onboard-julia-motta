fb523f44c0d035ad9118dd4c85207dc90176a3f8
Julia Motta <juliamottapaes@gmail.com>
2024-01-04 10:51:51 -0300

    usa formatador

---
+++ onboard-julia/App.tsx
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {SafeAreaView, Text, useColorScheme, View} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
