3639f8c3cbfaf453c0b9a6ef6f17aeaa5f6eec8f
Julia Motta <juliamottapaes@gmail.com>
2024-01-08 15:33:30 -0300

    create styles.tsx

--- onboard-julia/login-page.tsx
+++ onboard-julia/login-page.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {isValidEmail, isValidPassword} from './login-validation';
+import {styles} from './styles';
 import {gql} from '@apollo/client';
 import {client} from './apollo-client';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -8,22 +9,23 @@ import {
   View,
   Text,
   TextInput,
-  StyleSheet,
   TouchableOpacity,
   Alert,
+  ActivityIndicator,
 } from 'react-native';
+
+const LOGIN_USER = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    login(data: {email: $email, password: $password}) {
+      token
+    }
+  }
+`;
 
 export function Login(): React.JSX.Element {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-
-  const LOGIN_USER = gql`
-    mutation LoginUser($email: String!, $password: String!) {
-      login(data: {email: $email, password: $password}) {
-        token
-      }
-    }
-  `;
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleLoginPress = () => {
     if (!isValidEmail(email)) {
@@ -42,32 +44,34 @@ export function Login(): React.JSX.Element {
         'As senhas devem ter no mínimo uma letra e um número',
       );
       return;
-    } else {
-      client
-        .mutate({
-          mutation: LOGIN_USER,
-          variables: {
-            email,
-            password,
-          },
-        })
-        .then(async response => {
-          if (response.data === null) {
-            const error = response.errors ? response.errors[0] : null;
-            if (error) {
-              console.error('GraphQL Error:', error.message);
-              Alert.alert(`GraphQL Error: ${error.message}`);
-            }
-          } else {
-            const token = response.data.login.token;
-            await AsyncStorage.setItem('authToken', token);
-            console.log('Token: ', token);
-            Alert.alert('Sucesso', 'Login efetuado com sucesso.');
-          }
-        })
-        .catch(error => console.error(error));
-      return;
     }
+
+    setLoading(true);
+
+    client
+      .mutate({
+        mutation: LOGIN_USER,
+        variables: {
+          email,
+          password,
+        },
+      })
+      .then(async response => {
+        if (response.data === null) {
+          const error = response.errors ? response.errors[0] : null;
+          if (error) {
+            console.error('GraphQL Error:', error.message);
+            Alert.alert(`GraphQL Error: ${error.message}`);
+          }
+        } else {
+          const token = response.data.login.token;
+          await AsyncStorage.setItem('authToken', token);
+          console.log('Token: ', token);
+          Alert.alert('Sucesso', 'Login efetuado com sucesso.');
+        }
+      })
+      .catch(error => console.error(error))
+      .finally(() => setLoading(false));
   };
 
   return (
@@ -93,40 +97,8 @@ export function Login(): React.JSX.Element {
       <TouchableOpacity style={styles.button} onPress={handleLoginPress}>
         <Text style={styles.buttonText}>Entrar</Text>
       </TouchableOpacity>
+
+      {loading && <ActivityIndicator size="large" color="#0000ff" />}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 15,
-    width: '80%',
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginTop: '10%',
-    marginBottom: '20%',
-  },
-  button: {
-    width: '80%',
-    backgroundColor: '#BC8F8F',
-    padding: 10,
-    borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 15,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
