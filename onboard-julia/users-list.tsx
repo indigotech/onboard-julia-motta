971dd08ffcc163a32090798e4270a5e18538b5e8
Julia Motta <juliamottapaes@gmail.com>
2024-01-09 19:54:58 -0300

    resolve warnings

--- onboard-julia/users-list.tsx
+++ onboard-julia/users-list.tsx
@@ -62,7 +62,7 @@ export function UsersList(): React.JSX.Element {
 
       <FlatList
         data={usersList}
-        keyExtractor={user => user.id.toString()}
+        keyExtractor={(user, index) => `${user.id}_${index}`} // Ensure unique keys
         renderItem={({item}) => <UserItem user={item} />}
         onEndReached={handleEndReached}
         onEndReachedThreshold={0.1}
