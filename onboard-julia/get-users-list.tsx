3028c249056223133a93f98da1ece054df9e0365
Julia Motta <juliamottapaes@gmail.com>
2024-01-09 11:48:38 -0300

    add users list integration

---
+++ onboard-julia/get-users-list.tsx
@@ -0,0 +1,39 @@
+import {gql} from '@apollo/client';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {client} from './apollo-client';
+
+const GET_USERS = gql`
+  query GetUsers {
+    users {
+      nodes {
+        id
+        name
+        email
+      }
+    }
+  }
+`;
+
+export const getUsersList = async () => {
+  const token = await AsyncStorage.getItem('authToken');
+
+  try {
+    const {data} = await client.query({
+      query: GET_USERS,
+      context: {
+        headers: {
+          authorization: token,
+        },
+      },
+    });
+
+    if (data && data.users) {
+      return data.users;
+    } else {
+      return [];
+    }
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return [];
+  }
+};
